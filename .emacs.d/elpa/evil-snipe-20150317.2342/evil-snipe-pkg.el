ec03dcac9f75f1104863f037b3abfc77fa31312f
bounceme <mac@macs-mbp.attlocal.net>
2015-03-21 12:54:37 -0700

    deleting folder,adding ".emacs"

--- .emacs.d/elpa/evil-snipe-20150317.2342/evil-snipe-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-snipe" "20150317.2342" "emulate vim-sneak & vim-seek" '((evil "1.0.9")) :url "https://github.com/hlissner/evil-snipe" :keywords '("emulation" "vim" "evil" "sneak" "seek"))
